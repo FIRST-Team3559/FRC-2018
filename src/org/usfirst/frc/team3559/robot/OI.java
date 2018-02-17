0eb092247ee95e39a3ce290d48b1881df4291e7e
arrawlins22 <35584142+arrawlins22@users.noreply.github.com>
2018-02-17 11:51:11 -0500

    2/17/18

--- src/org/usfirst/frc/team3559/robot/OI.java
+++ src/org/usfirst/frc/team3559/robot/OI.java
@@ -7,6 +7,11 @@
 
 package org.usfirst.frc.team3559.robot;
 
+import org.usfirst.frc.team3559.robot.commands.*;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.*;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
@@ -39,4 +44,66 @@ public class OI {
 	// Start the command when the button is released and let it run the command
 	// until it is finished as determined by it's isFinished method.
 	// button.whenReleased(new ExampleCommand());
-}
+		private Joystick gamepad = new Joystick (0);
+		private Joystick gamepad2 = new Joystick (1);
+		Button lTrigger = new JoystickButton(gamepad, 5);
+		Button rTrigger = new JoystickButton(gamepad, 6);
+		Button greenA2 = new JoystickButton(gamepad2, 1);
+		Button redB2 = new JoystickButton(gamepad2, 2);
+		Button yellowY2 = new JoystickButton(gamepad2, 4);
+		Button blueX2 = new JoystickButton(gamepad2, 3);
+		
+		public OI() {
+			lTrigger.whileHeld(new SloGear());
+			rTrigger.whileHeld(new HiGear());
+			greenA2.whenPressed(new ChangeColor());
+		}
+
+
+		public Joystick getJoystick() {
+			return gamepad;
+		}
+		public int DPad() {
+		    return gamepad.getPOV(0);
+		}
+
+		/**
+		 * @return True if the DPad is pushed right, False if it is not pressed
+		 */
+		public boolean DPadRight() {
+		  if (gamepad.getPOV(0) == 90) {
+		        return true;
+		  }else {
+		        return false;
+		}
+		}
+
+		/**
+		 * @return True if the DPad is pushed down, False if it is not pressed
+		 */
+		public boolean DPadDown() {
+		    if (gamepad.getPOV(0) == 180) {
+		        return true;
+		    }else {
+		        return false;
+		}
+		}
+		public boolean DPadUp() {
+		    if (gamepad.getPOV(0) == 0) {
+		        return true;
+		    }else {
+		        return false;
+		}
+		}
+
+		/**
+		 * @return True if the DPad is pushed left, False if it is not pressed
+		 */
+		public boolean DPadLeft() {
+		    if (gamepad.getPOV(0) == 270 ) {
+		        return true;
+		    } else {
+		        return false;
+		    }
+		}
+		}
--- src/org/usfirst/frc/team3559/robot/Robot.java
+++ src/org/usfirst/frc/team3559/robot/Robot.java
@@ -12,9 +12,10 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.usfirst.frc.team3559.robot.commands.ExampleCommand;
-import org.usfirst.frc.team3559.robot.subsystems.ExampleSubsystem;
 
+import org.usfirst.frc.team3559.robot.commands.NoAuto;
+import org.usfirst.frc.team3559.robot.subsystems.Blinkin;
+import org.usfirst.frc.team3559.robot.subsystems.DriveBase;
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
@@ -23,8 +24,8 @@ import org.usfirst.frc.team3559.robot.subsystems.ExampleSubsystem;
  * project.
  */
 public class Robot extends TimedRobot {
-	public static final ExampleSubsystem kExampleSubsystem
-			= new ExampleSubsystem();
+	public static final DriveBase drivebase = new DriveBase();
+	public static final Blinkin blinkin = new Blinkin();
 	public static OI m_oi;
 
 	Command m_autonomousCommand;
@@ -37,7 +38,7 @@ public class Robot extends TimedRobot {
 	@Override
 	public void robotInit() {
 		m_oi = new OI();
-		m_chooser.addDefault("Default Auto", new ExampleCommand());
+		m_chooser.addDefault("Default Auto", new NoAuto());
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", m_chooser);
 	}
