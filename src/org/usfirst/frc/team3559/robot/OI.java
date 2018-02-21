c660b5e0d3c4931ab9c8ff73696260afa265ed36
arrawlins22 <35584142+arrawlins22@users.noreply.github.com>
2018-02-20 20:43:19 -0500

    stripped everything but drive code

--- src/org/usfirst/frc/team3559/robot/OI.java
+++ src/org/usfirst/frc/team3559/robot/OI.java
@@ -17,96 +17,19 @@ import edu.wpi.first.wpilibj.buttons.*;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	//// CREATING BUTTONS
-	// One type of button is a joystick button which is any button on a
-	//// joystick.
-	// You create one by telling it which joystick it's on and which button
-	// number it is.
-	// Joystick stick = new Joystick(port);
-	// Button button = new JoystickButton(stick, buttonNumber);
-
-	// There are a few additional built in buttons you can use. Additionally,
-	// by subclassing Button you can create custom triggers and bind those to
-	// commands the same as any other Button.
-
-	//// TRIGGERING COMMANDS WITH BUTTONS
-	// Once you have a button, it's trivial to bind it to a button in one of
-	// three ways:
-
-	// Start the command when the button is pressed and let it run the command
-	// until it is finished as determined by it's isFinished method.
-	// button.whenPressed(new ExampleCommand());
-
-	// Run the command while the button is being held down and interrupt it once
-	// the button is released.
-	// button.whileHeld(new ExampleCommand());
-
-	// Start the command when the button is released and let it run the command
-	// until it is finished as determined by it's isFinished method.
-	// button.whenReleased(new ExampleCommand());
 		Joystick gamepad = new Joystick (0);
 		Joystick gamepad2 = new Joystick (1);
-		Button lTrigger = new JoystickButton(gamepad, 5);
-		Button rTrigger = new JoystickButton(gamepad, 6);
-		Button greenA2 = new JoystickButton(gamepad2, 1);
-		Button redB2 = new JoystickButton(gamepad2, 2);
-		Button yellowY2 = new JoystickButton(gamepad2, 4);
-		Button blueX2 = new JoystickButton(gamepad2, 3);
+		Button lShoulder = new JoystickButton(gamepad, 5);
+		Button rShoulder = new JoystickButton(gamepad, 6);
+	
 		
 		public OI() {
-			lTrigger.whileHeld(new SloGear());
-			rTrigger.whileHeld(new HiGear());
-			greenA2.whenPressed(new LowShoot());
-			redB2.whenPressed(new MidShoot());
-			yellowY2.whenPressed(new HiShoot());
-			
+			lShoulder.whileHeld(new SloGear());
+			rShoulder.whileHeld(new HiGear());
 		}
 
 
 		public Joystick getJoystick() {
 			return gamepad;
 		}
-		public int DPad() {
-		    return gamepad.getPOV(0);
-		}
-
-		/**
-		 * @return True if the DPad is pushed right, False if it is not pressed
-		 */
-		public boolean DPadRight() {
-		  if (gamepad.getPOV(0) == 90) {
-		        return true;
-		  }else {
-		        return false;
-		}
-		}
-
-		/**
-		 * @return True if the DPad is pushed down, False if it is not pressed
-		 */
-		public boolean DPadDown() {
-		    if (gamepad.getPOV(0) == 180) {
-		        return true;
-		    }else {
-		        return false;
-		}
-		}
-		public boolean DPadUp() {
-		    if (gamepad.getPOV(0) == 0) {
-		        return true;
-		    }else {
-		        return false;
-		}
-		}
-
-		/**
-		 * @return True if the DPad is pushed left, False if it is not pressed
-		 */
-		public boolean DPadLeft() {
-		    if (gamepad.getPOV(0) == 270 ) {
-		        return true;
-		    } else {
-		        return false;
-		    }
-		}
-		}
+}
