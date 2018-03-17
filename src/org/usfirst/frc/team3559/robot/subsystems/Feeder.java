198e6c31f7e409241357ac82a57b446cf26bb4c9
Fred Routson <froutson@gmail.com>
2018-03-17 15:34:07 -0400

    Revert "Remove Camera Servo and edited feeder subsystem"
    This reverts commit e837faf11ec4c5bd85707c9598711d4a31722442.

--- src/org/usfirst/frc/team3559/robot/subsystems/Feeder.java
+++ src/org/usfirst/frc/team3559/robot/subsystems/Feeder.java
@@ -8,60 +8,51 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class Feeder extends Subsystem {
-	private Spark FlipMotor = new Spark(7);
-	private Spark feedMotorLeft = new Spark(5);
-	private Spark feedMotorRight = new Spark(6);
-	public boolean Lifted = false;
+	private Spark FlipMotor = new Spark(9);
+	private Spark Feed1 = new Spark(10);
+	private Spark Feed2 = new Spark(11);
+	public boolean Lift = false;
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
-    public void FeedIn() {
-    	feedMotorLeft.set(0.5);
-    	feedMotorRight.set(0.5);
-    	Timer.delay(0.5);
-    	feedMotorLeft.set(0.0);
-    	feedMotorRight.set(0.0);
-    }
 
-    public void FeedOut() {
-    	feedMotorLeft.set(-0.5);
-    	feedMotorRight.set(-0.5);
-    	Timer.delay(0.5);
-    	feedMotorLeft.set(0.0);
-    	feedMotorRight.set(0.0);
-    }
-
-    public void Lift() {
-    	FlipMotor.set(0.6);
-    	Timer.delay(1.0);
-    	FlipMotor.set(0.0);
-    	Lifted = true;
-    }
-    public void Lower() {
-    	FlipMotor.set(-0.6);
-    	Timer.delay(1.0);
-    	FlipMotor.set(0.0);
-    	Lifted = false;
-    }
-    /***************
-     * Make a determiniation on position of intake and reverse the postion.
-     * This should be modified to add limit switches and position detection.
-     */
-    public void Judge() {
-    	if(Lifted == true) {
-    		Lower();
-    	}else {
-    		Lift();	
+public void Lift() {
+	Feed1.set(0.5);
+	Feed2.set(0.5);
+	Timer.delay(0.5);
+	Feed1.set(0.0);
+	Feed2.set(0.0);
+	Timer.delay(0.1);
+	FlipMotor.set(0.6);
+	Timer.delay(1.0);
+	FlipMotor.set(0.0);
+	Lift = true;
+}
+public void Lower() {
+	Feed1.set(0.5);
+	Feed2.set(0.5);
+	Timer.delay(0.5);
+	Feed1.set(0.0);
+	Feed2.set(0.0);
+	Timer.delay(0.1);
+	FlipMotor.set(-0.6);
+	Timer.delay(1.0);
+	FlipMotor.set(0.0);
+	Lift = false;
+}
+public void Judge() {
+	if(Lift = true) {
+		Lower();
+	}else {
+	if(Lift = false) {
+		Lift();
+					}	
 			}
 	}
-    
-    public void StopAll() {
-    	FlipMotor.set(0.0);
-    	feedMotorLeft.set(0.0);
-    	feedMotorRight.set(0.0);
-    	
-    }
 
 }
